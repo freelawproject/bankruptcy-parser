069d8aae92a3bab67123f52f7a4588a13b41eff7
vvilliam <wpalin@gmail.com>
2021-02-25 11:06:22 -0500

    fix(init): Make imports better

---
+++ bankruptcy/__init__.py
@@ -0,0 +1,8 @@
+# __init__.py
+from .parser import (
+    extract_all,
+    extract_official_form_106_a_b,
+    extract_official_form_106_d,
+    extract_official_form_106_e_f,
+    extract_official_form_106_sum,
+)
